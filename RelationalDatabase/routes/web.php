f26cfcdece87603cc57839e58b2cd2fa2097c070
Mynul436 <mynulakash436@gmail.com>
2023-02-13 17:28:14 +0600

    task1 added

--- RelationalDatabase/routes/web.php
+++ RelationalDatabase/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CsvController;
+use App\Http\Controllers\PostController;
+
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +26,14 @@ Route::get('/', function () {
 Route::get('addcustomer',[CustomerController::class,'add_customer']);
 
 Route::get('showmobile/{id}',[CustomerController::class,'show_mobile']);
+
+Route::get('add-author',[AuthorController::class,'add_author']);
+Route::get('add-post/{id}',[PostController::class,'add_post']);
+Route::get('show-post/{id}',[PostController::class,'show_post']);
+Route::get('show-author/{id}',[AuthorController::class,'show_author']);
+Route::get('index/{id}',[IndexController::class,'index']);
+
+Route::get('comment/{id}',[CommentController::class,'add_comment']);
+
+//Route::get('show-comment/{id}/{id1}',[CommentController::class,'show_comment']);
+Route::get("data", [CsvControllercd::class, "index"]);
